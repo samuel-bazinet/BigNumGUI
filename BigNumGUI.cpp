46a2fee96a5cb1da2ef467f6fe0618f60888145e
Samuel Bazinet <bazinet.samuel1@gmail.com>
2022-01-13 20:51:19 -0500

    Added comments, added to the README.md and created a txt version of the README

--- BigNumGUI.cpp
+++ BigNumGUI.cpp
@@ -8,6 +8,7 @@
 #include <map>
 #include "LLBigNum.h"
 
+// we set up global variables that will be used by the functions
 std::map<std::string, BigNum> container;
 
 Fl_Input inputName(150, 10, 180, 40, "Number name: ");
@@ -16,79 +17,133 @@ Fl_Output output(150, 330, 180, 40, "Output : ");
 Fl_Browser browser(150, 110, 180, 200);
 short counter = 0;
 
+/**
+ * @brief This button function will add a number to the browser and the map
+ * 
+ * @param buttonptr The button calling the function
+ */
 void buttonAddBN_cb(Fl_Widget* buttonptr) {
 
+    // the number is inserted into the map using the user chosen name as the key
     container.insert(std::pair<std::string, BigNum>(inputName.value(), BigNum(std::stoi(inputValue.value()))));
+
+    // the name is added to the browser
     browser.insert(++counter, inputName.value());
+
+    // the input fields are cleared
     inputName.value("");
     inputValue.value("");
     
 
 }
 
+/**
+ * @brief This button adds a + to the browser
+ * 
+ * @param buttonptr  The button calling the function
+ */
 void buttonAddOperator_cb(Fl_Widget* buttonptr) {
 
+    // the + operator is added to the browser
     browser.insert(++counter, "+");
 
 }
 
+/**
+ * @brief This button adds a * to the browser
+ * 
+ * @param buttonptr  The button calling the function
+ */
 void buttonMulOperator_cb(Fl_Widget* buttonptr) {
 
+    // the * operator is added to the browser
     browser.insert(++counter, "*");
 
 }
 
+/**
+ * @brief This button adds a ! to the browser
+ * 
+ * @param buttonptr The button calling the function
+ */
 void buttonFactOperator_cb(Fl_Widget* buttonptr) {
 
+    // the ! operator is added to the browser
     browser.insert(++counter, "!");
 
 }
 
+/**
+ * @brief This button calculates the operation defined in the browser 
+ * 
+ * @param buttonptr The button calling the function
+ */
 void buttonCalculate_cb(Fl_Widget* buttonptr) {
 
+    // a stack is used to store the numbers for the RPN style operation
     std::stack<BigNum> numbers;
 
+    // this string will be used to store the names coming out of the browser
     std::string browserOut;
 
+
+    // a for loop is used to iterate through the browser
     for (short i = 1; i <= counter; i++) {
 
+        // the text at the line is extracted
         browserOut = browser.text(i);
 
+        // the text is matched against the operators
         if (browserOut == "+") {
 
+            // if it is a +, then the first 2 elements from the stack are popped, then the addition is performed
             BigNum a = numbers.top();
             numbers.pop();
             BigNum b = numbers.top();
             numbers.pop();
+            // the result of the addition is pushed back onto the stack
             numbers.push(a + b);
 
         } else if (browserOut == "*") {
 
+            // if it is a *, then the first 2 elements from the stack are popped, then the multiplication is performed
             BigNum a = numbers.top();
             numbers.pop();
             BigNum b = numbers.top();
             numbers.pop();
+            // the result of the multiplication is pushed back onto the stack
             numbers.push(a * b);
 
         } else if (browserOut == "!") {
-
+            
+            // if it is a !, then the first element is popped from the stack, then the factorial is performed
             BigNum a = numbers.top();
             numbers.pop();
+            // the result is pushed back onto the stack
             numbers.push(factorial(a));
 
         } else {
 
+            // any number is pushed onto the stack
             numbers.push(container.at(browserOut));
 
         }
 
     }
+
+    // the remaining element of the stack is outputted in the output field
     output.value(numbers.top().toString().c_str());
 
 }
 
+/**
+ * @brief This button clears the fields 
+ * 
+ * @param buttonptr The button calling the function
+ */
 void buttonClear_cb(Fl_Widget* buttonptr) {
 
+    // the browser, output, map and counter are reset
     browser.clear();
     output.value("");
     container = std::map<std::string, BigNum>();
@@ -109,6 +164,7 @@ int main() {
         win.add(output);
         win.add(browser);
 
+        // adding all of the buttons
         Fl_Button *buttonAddBN = new Fl_Button(360, 10, 140, 40, "Add Number");
         buttonAddBN->callback(buttonAddBN_cb);
 
@@ -132,5 +188,6 @@ int main() {
 
     win.show();
 
+    // running the UI
     return Fl::run();
 }
--- README.txt
+++ README.txt
@@ -1,12 +1,12 @@
-# BigNumGUI
+BigNumGUI
 
 This project is about making a GUI to interact with my BigNum class
 
-## About
+About:
 - This program demonstrates some features of my BigNum class
 - FLTK is used to make the GUI element of the program.
 
-## How to run
+How to run
 - You will need to have a linux OS, VM running linux, or WSL running on your machine to use this (Tested on Linux Mint (Ubuntu based))
 - If you have fltk 1.3.x installed on your machine (link to download page: https://www.fltk.org/software.php)
     - Simply open your terminal
@@ -14,12 +14,12 @@ This project is about making a GUI to interact with my BigNum class
     - Run the **make** command
     - Then run **./a.out** to run the program
 
-## How to use:
-1. Enter a name for a number
-2. Enter a number
-3. Repeat if deisred
-4. Press of the operator buttons
+How to use:
+- Enter a name for a number
+- Enter a number
+- Repeat if deisred
+- Press of the operator buttons
     - Make sure that the number of operator is matching the operators 
-5. Press the calculate button for the calculations to happen
-6. The result will be in the output
-7. Press the Clear button to clear the fields
+- Press the calculate button for the calculations to happen
+- The result will be in the output
+- Press the Clear button to clear the fields
